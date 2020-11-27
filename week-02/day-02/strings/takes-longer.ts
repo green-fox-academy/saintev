5ae94c19f77a30bd22820a82cd14b44f39cf799b
Eva Szentmiklosi <eszent@gmail.com>
2020-11-27 18:49:25 +0100

    Finished first four Strings excercises

---
+++ week-02/day-02/strings/takes-longer.ts
@@ -0,0 +1,14 @@
+'use strict';
+
+// While saving this quote, a disk error has occured. Please fix it.
+// Add "always takes longer than" between the words "It" and "you"
+
+let quote: string = `Hofstadter's Law: It you expect, even when you take into account Hofstadter's Law.`;
+
+let x: number = quote.indexOf('you');
+console.log(quote.indexOf('you'));
+
+
+quote = quote.slice(0, x - 1) + ' always takes longer than ' + quote.slice(x);
+
+console.log(quote);
--- week-02/day-02/strings/url-fixer.ts
+++ week-02/day-02/strings/url-fixer.ts
@@ -1,15 +1,15 @@
 'use strict';
 
-
 // Accidentally I got the wrong URL for a funny subreddit. It's probably "odds" and not "bots"
+
 let url: string = 'https//www.reddit.com/r/nevertellmethebots';
 url.replace ('bots','odds');
 
 // Also, the URL is missing a crucial component, find out what it is and insert it too!
 
 //ketfele vagjuk
-let sliced1: any = url.slice(0, 5);
-let sliced2: string = url.slice(5, -1);
+let sliced1: string = url.slice(0, 5);
+let sliced2: string = url.slice(5); // a masodik helyre lehet irni azt is, hogy hatulrol a hanyadik, pl -1
 
 console.log(sliced1);
 console.log(sliced2);
