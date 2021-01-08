6c1bca66f2d31b09327101955022d9e4ccc62571
Eva Szentmiklosi <eszent@gmail.com>
2021-01-08 21:34:59 +0100

    onclick button is working on left button

--- week-06/day-05/mygallery/gallery.js
+++ week-06/day-05/mygallery/gallery.js
@@ -1,15 +1,18 @@
 'use strict';
 
-const right = document.querySelector( 'button' );
+const button = document.querySelector( 'button' );
+const counter = document.querySelector( '.counter' );
 
-let images = [
-  './images/jellyfish.jpg',
-  './images/mother.jpg',
-  './images/bird.jpg'
-]
 
-let imgUrl = document.querySelector( 'bigpicture' );
+//let images = [
+//  './images/jellyfish.jpg',
+//  './images/mother.jpg',
+//  './images/bird.jpg'
+//]
 
-right.onclick = () => {
-  imgUrl.setAttribute('src', './images/mother.jpg');
+let imgUrl = document.querySelector( '#bigpicture' );
+
+button.onclick = () => {
+  
+  imgUrl.setAttribute('src', './images/mother.jpg')  
 }
