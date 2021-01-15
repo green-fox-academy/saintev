1a43b51a79020e61158bf3d0cc3a6850dc7da1f7
Eva Szentmiklosi <eszent@gmail.com>
2021-01-15 18:56:20 +0100

    changed to add multiple querys - still not working

--- week-07/day-04/bookstore/server.js
+++ week-07/day-04/bookstore/server.js
@@ -34,38 +34,43 @@ app.get( '/booktitle', ( req, res ) => {
 
 app.get( '/allbookinfo', ( req, res ) => {
   
-  let bookCategory = '*'
+  let mainQuery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast 
+ JOIN author ON book_mast.aut_id = author.aut_id 
+ JOIN category ON book_mast.cate_id = category.cate_id
+ JOIN publisher ON book_mast.pub_id = publisher.pub_id`
+  let searchWords = [];
+
+  let bookCategory = ' '
   if ( req.query.category ) {
     bookCategory = req.query.category;
-    console.log(bookCategory);
+    console.log( bookCategory );
+    mainQuery += ` WHERE cate_descrip = (?)`
+    searchWords.push( bookCategory );
   };
 
-  let bookPublisher = '*';
+  let bookPublisher = ' '; 
   if ( req.query.publisher ) {
-    bookPublisher = `%${ req.query.publisher }%`;
+    bookPublisher = req.query.publisher;
+    mainQuery += ` AND pub_name = (?);`
+    searchWords.push( bookPublisher );
   }
   
   let plt = 2000;
   if ( req.query.plt ) {
     plt = req.query.plt;
+    mainQuery += `WHERE book_price < 2000;`
+    
   }
     
   let pgt = 0;
   if ( req.query.pgt ) {
   pgt = req.query.pgt;
-
   }
-  
-  conn.query( `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast 
- JOIN author ON book_mast.aut_id = author.aut_id 
- JOIN category ON book_mast.cate_id = category.cate_id
- JOIN publisher ON book_mast.pub_id = publisher.pub_id
- WHERE cate_descrip = (?)
- & pub_name = (?)
- & book_price < (?)
- & book_price > (?);`
-    
- , [ bookCategory, bookPublisher, plt, pgt ], ( err, rows ) => {
+   
+ //& book_price < (?)
+ //& book_price > (?);
+
+  conn.query( mainQuery , searchWords , ( err, rows ) => { 
     if ( err ) {
       console.log( err.toString() );
       res.status( 500 ).json( { 'error': 'database error' } );
