58fff5bd49a1800d27967a9041940cb5ea234d10
Eva Szentmiklosi <eszent@gmail.com>
2021-01-15 01:29:39 +0100

    added bookstore exercise

--- week-07/day-04/bookstore/server.js
+++ week-07/day-04/bookstore/server.js
@@ -33,13 +33,39 @@ app.get( '/booktitle', ( req, res ) => {
 } );
 
 app.get( '/allbookinfo', ( req, res ) => {
-  let myQuery = 
-  `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast 
-  JOIN author ON book_mast.aut_id = author.aut_id 
-  JOIN category ON book_mast.cate_id = category.cate_id
-  JOIN publisher ON book_mast.pub_id = publisher.pub_id; `;
   
-  conn.query( myQuery, ( err, rows ) => {
+  let bookCategory = '*'
+  if ( req.query.category ) {
+    bookCategory = req.query.category;
+    console.log(bookCategory);
+  };
+
+  let bookPublisher = '*';
+  if ( req.query.publisher ) {
+    bookPublisher = `%${ req.query.publisher }%`;
+  }
+  
+  let plt = 2000;
+  if ( req.query.plt ) {
+    plt = req.query.plt;
+  }
+    
+  let pgt = 0;
+  if ( req.query.pgt ) {
+  pgt = req.query.pgt;
+
+  }
+  
+  conn.query( `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast 
+ JOIN author ON book_mast.aut_id = author.aut_id 
+ JOIN category ON book_mast.cate_id = category.cate_id
+ JOIN publisher ON book_mast.pub_id = publisher.pub_id
+ WHERE cate_descrip = (?)
+ & pub_name = (?)
+ & book_price < (?)
+ & book_price > (?);`
+    
+ , [ bookCategory, bookPublisher, plt, pgt ], ( err, rows ) => {
     if ( err ) {
       console.log( err.toString() );
       res.status( 500 ).json( { 'error': 'database error' } );
@@ -47,7 +73,7 @@ app.get( '/allbookinfo', ( req, res ) => {
     }
     res.json( rows );
   } );
+  
 } );
-
 
 app.listen(3000);
