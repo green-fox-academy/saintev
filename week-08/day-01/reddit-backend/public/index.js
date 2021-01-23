a41e06cd8c75d8456cb8460c7dcca537abebf62d
Eva Szentmiklosi <eszent@gmail.com>
2021-01-24 00:00:07 +0100

    added some optional endpoints

---
+++ week-08/day-01/reddit-backend/public/index.js
@@ -0,0 +1,29 @@
+window.onload = () => {
+  let linkRequest = new XMLHttpRequest(); //hasznalja ezt az adatbazist
+  linkRequest.onreadystatechange = handleReadyStateChange;
+  
+  linkRequest.open( 'GET', 'http://localhost:3000/posts', false ); //a server.js kap egy lekerest erre az endpointra akkor 
+  //megnyitja ezt a csatornat
+  linkRequest.send(); // ezzel megy el az adat valojaban, ezzel kuldjuk el a lekerest
+
+  function handleReadyStateChange () {
+    //if ( linkRequest.readyState === 4 ) { // 4 helyett: XMLHttpRequest.DONE
+    //  console.log(linkRequest.response);
+    //}
+    linkRequest.onload = () => { // ez ugyanaz mint a fenti ketto
+      //console.log(linkRequest.response);
+      
+      let linkObject = JSON.parse( linkRequest.response );//stringbol alakitsa at, legyen egy array
+        linkObject.forEach( link => {
+        addLinkToList( link );
+      } );
+    }
+  }
+}
+const linkList = document.querySelector( 'ul' );
+const addLinkToList = ( link ) => {
+  const row = document.createElement ('li' );
+  row.innerHTML = `id: ${link.id} : <strong>${ link.title }:</strong> ${ link.url }`;
+  linkList.appendChild( row );
+  console.log(row);
+}
--- week-08/day-01/reddit-backend/server.js
+++ week-08/day-01/reddit-backend/server.js
@@ -1,12 +1,11 @@
 'use strict'
 
-const { SSL_OP_NO_SESSION_RESUMPTION_ON_RENEGOTIATION } = require( 'constants' );
 const express = require( 'express' );
 const mysql = require( 'mysql' );
 const app = express();
 app.use( express.json() );
-app.use( express.urlencoded( { extended: true } ) );
 app.use( express.static( 'public' ) );//a public mappabol behuzza majd a htmlt a css es js -el egyutt
+app.use(express.urlencoded({ extended: true }));//ha csak siman html-formot hasznalunk, mert nem json-ben kuldi az adatot a req.bodyban
 const PORT = 3000;
 
 const conn = mysql.createConnection( {
@@ -40,16 +39,16 @@ app.get( '/posts', ( req, res ) => {
 
 app.post( '/posts', ( req, res ) => {
   conn.query( `INSERT INTO posts (title,url)
-  VALUES(?,?)`, [ req.body.title, req.body.url ], ( err, rows ) => {
+  VALUES(?,?)`, [ req.body.title, req.body.url ], ( err ) => {
     if ( err ) {
       console.log( err.toString() );
       res.status( 500 ).json( { 'error': 'database error' } );
       return;
     }
     if ( req.body.title && req.body.url ) {
-      res.send( 'Ok!post added!' );
-      res.json( rows );
-      //res.redirect( '/' );
+      //res.send( 'Ok!post added!' );
+      //res.json( rows );
+      res.redirect( '/' );
     } else
         res.send('Please provide a title and a valid url!')
       return;
@@ -63,7 +62,8 @@ app.put( '/posts/:id/upvote', ( req, res ) => {
       res.status( 500 ).json( { 'error': 'database error' } );
       return;
     }
-    res.status( 200 ).json('ok' );
+    res.status( 200 ).json( 'ok' );
+    //res.redirect('/');
   } );
 } );
 
@@ -78,9 +78,27 @@ app.put( '/posts/:id/downvote', ( req, res ) => {
   } );
 } );
 
+app.delete( '/posts/:id', ( req, res ) => {
+  conn.query( `DELETE FROM posts WHERE id = ${ req.params.id }`, ( err ) => {
+    if ( err ) {
+      console.log( err.toString() );
+      res.status( 500 ).json( { 'error': 'database error' } );
+      return;
+    }
+    //res.status( 200 ).json(rows); // nem tudom milyen rowst ad vissza
+    res.status( 200 ).json( 'ok, deleted!' )
+  } );
+} );
 
-  
-
+app.put( '/posts/:id', ( req, res ) => {
+  conn.query( `UPDATE posts SET title = '${req.body.title}', url = '${req.body.url}' WHERE id = ${ req.params.id }`, ( err, rows ) => {
+    if ( err ) {
+      console.log( err.toString() );
+      res.status( 500 ).json( { 'error': 'database error' } );
+    }
+    res.status( 200 ).json( rows );
+  } );
+} );
 
 app.listen( PORT, () => {
   console.log('Listening to port 3000');
