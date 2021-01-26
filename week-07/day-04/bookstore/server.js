29c96542f1a571ab5cb1b2dc55fb996ee0498d77
Eva Szentmiklosi <eszent@gmail.com>
2021-01-26 17:13:07 +0100

    simplified multiple search

--- week-07/day-04/bookstore/server.js
+++ week-07/day-04/bookstore/server.js
@@ -37,39 +37,28 @@ app.get( '/allbookinfo', ( req, res ) => {
   let mainQuery = `SELECT book_name, aut_name, cate_descrip, pub_name, book_price FROM book_mast 
  JOIN author ON book_mast.aut_id = author.aut_id 
  JOIN category ON book_mast.cate_id = category.cate_id
- JOIN publisher ON book_mast.pub_id = publisher.pub_id`
+ JOIN publisher ON book_mast.pub_id = publisher.pub_id
+ WHERE book_price > -1 `
   
   let searchWords = [];
   
   if ( req.query.category ) {
-    mainQuery += ` WHERE cate_descrip = (?)`
+    mainQuery += ` AND cate_descrip = (?)`
     searchWords.push( req.query.category );
   };
 
   if ( req.query.publisher ) {
-    if ( req.query.category && req.query.publisher ) {
     mainQuery += ` AND pub_name = (?)`
-    } else {
-    mainQuery += ` WHERE pub_name = (?)`
-    }
     searchWords.push( req.query.publisher );
   }
   
   if ( req.query.plt ) {
-    if ( req.query.category || req.query.publisher ) {
     mainQuery += ` AND book_price < (?)`
-    } else {
-    mainQuery += ` WHERE book_price < (?)`
-    }
     searchWords.push(req.query.plt)
   }
     
   if ( req.query.pgt ) {
-    if ( req.query.category || req.query.publisher || req.query.plt ) {
     mainQuery += ` AND book_price > (?)` 
-    } else {
-    mainQuery += ` WHERE book_price > (?)`
-    }
     searchWords.push( req.query.pgt );
   }
    
